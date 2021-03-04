e38d879daed169aa345ecd9944a69acbe3e3d4c7
Riaz Abed <Riaz.Abed@standardbank.co.za>
2021-03-04 11:02:18 +0200

    Added update customer test

--- src/test/java/com/example/technicalassessment/CustomerResource/CustomerResourceTests.java
+++ src/test/java/com/example/technicalassessment/CustomerResource/CustomerResourceTests.java
@@ -16,8 +16,11 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -28,13 +31,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 public class CustomerResourceTests {
 
+    private final String rootContext = "/customer";
     @Autowired
     CustomerRepository customerRepository;
-
     @Autowired
     private MockMvc mvc;
-
-    private final String rootContext = "/customer";
 
     @Before
     public void setUp() throws Exception {
@@ -42,12 +43,12 @@ public class CustomerResourceTests {
     }
 
     @Test
-    public void TestInsertCustomer() throws Exception {
+    public void testInsertCustomer() throws Exception {
         int collectionSize = customerRepository.findAll().size();
         Customer newCustomer = new Customer("Jack", "Black", "1 zebra street", true);
 
 
-        MockHttpServletRequestBuilder requestBuilder = put(rootContext +"/").contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON).content(asJson(newCustomer));
+        MockHttpServletRequestBuilder requestBuilder = put(rootContext + "/").contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON).content(asJson(newCustomer));
         mvc
                 .perform(requestBuilder)
                 .andExpect(status().isCreated())
@@ -55,6 +56,24 @@ public class CustomerResourceTests {
                 .andReturn();
 
         assertTrue(customerRepository.findAll().size() > collectionSize);
+    }
+
+    @Test
+    public void testUpdateCustomer() throws Exception {
+        Customer newCustomer = customerRepository.findAll().get(0);
+        newCustomer.setCustomerFirstName("New first name");
+        newCustomer.setCustomerLastName("New last name");
+
+        MockHttpServletRequestBuilder requestBuilder = post(rootContext + "/").contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON).content(asJson(newCustomer));
+        mvc
+                .perform(requestBuilder)
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andReturn();
+
+        assertEquals(customerRepository.findAll().get(0).getCustomerFirstName(),"New first name");
+        assertEquals(customerRepository.findAll().get(0).getCustomerLastName(),"New last name");
+        assertNotEquals(customerRepository.findAll().get(0).getCustomerFirstName(),"Tom");
     }
 
 
