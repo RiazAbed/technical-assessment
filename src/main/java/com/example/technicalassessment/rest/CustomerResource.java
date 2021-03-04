6a8af710e65f10e596d725ad027e3a55aefe8369
Riaz Abed <Riaz.Abed@standardbank.co.za>
2021-03-04 09:07:44 +0200

    Create test for InsertCustomer & rest endpoint for insertCustomer

--- src/main/java/com/example/technicalassessment/rest/CustomerResource.java
+++ src/main/java/com/example/technicalassessment/rest/CustomerResource.java
@@ -1,4 +1,27 @@
 package com.example.technicalassessment.rest;
 
+import com.example.technicalassessment.models.Customer;
+import com.example.technicalassessment.service.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@RestController
+@RequestMapping("/customer")
 public class CustomerResource {
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @PutMapping("/")
+    public ResponseEntity<Customer> insertCustomer(@RequestBody Customer customer) throws URISyntaxException {
+        Customer persistedCustomer = customerRepository.save(customer);
+        return ResponseEntity.created(new URI("/customer/" + customer.getId())).body(persistedCustomer);
+    }
+
 }
--- src/test/java/com/example/technicalassessment/CustomerResource/CustomerResourceTests.java
+++ src/test/java/com/example/technicalassessment/CustomerResource/CustomerResourceTests.java
@@ -1,4 +1,65 @@
 package com.example.technicalassessment.CustomerResource;
 
+import com.example.technicalassessment.models.Customer;
+import com.example.technicalassessment.service.CustomerRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.AutoConfigureDataMongo;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+
+import static org.junit.Assert.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureDataMongo
+@AutoConfigureMockMvc
 public class CustomerResourceTests {
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Autowired
+    private MockMvc mvc;
+
+    private final String rootContext = "/customer";
+
+    @Before
+    public void setUp() throws Exception {
+        customerRepository.save(new Customer("Tom", "Topper", "21 baker street", true));
+    }
+
+    @Test
+    public void TestInsertCustomer() throws Exception {
+        int collectionSize = customerRepository.findAll().size();
+        Customer newCustomer = new Customer("Jack", "Black", "1 zebra street", true);
+
+
+        MockHttpServletRequestBuilder requestBuilder = put(rootContext +"/").contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON).content(asJson(newCustomer));
+        mvc
+                .perform(requestBuilder)
+                .andExpect(status().isCreated())
+                .andDo(print())
+                .andReturn();
+
+        assertTrue(customerRepository.findAll().size() > collectionSize);
+    }
+
+
+    private String asJson(Object object) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.writeValueAsString(object);
+    }
 }
