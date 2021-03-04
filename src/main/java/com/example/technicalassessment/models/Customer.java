17996ec08de1560373d2eb349828ec6fedb93bd3
Riaz Abed <Riaz.Abed@standardbank.co.za>
2021-03-04 09:01:31 +0200

    Create customer entity

--- src/main/java/com/example/technicalassessment/models/Customer.java
+++ src/main/java/com/example/technicalassessment/models/Customer.java
@@ -1,4 +1,26 @@
 package com.example.technicalassessment.models;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+
+@Getter
+@Setter
 public class Customer {
+
+    @Id
+    private String id;
+
+    private String customerFirstName;
+    private String customerLastName;
+    private String address;
+    private boolean customerStatus;
+
+    public Customer(String customerFirstName, String customerLastName, String address, boolean customerStatus) {
+        this.customerFirstName = customerFirstName;
+        this.customerLastName = customerLastName;
+        this.address = address;
+        this.customerStatus = customerStatus;
+    }
 }
+
